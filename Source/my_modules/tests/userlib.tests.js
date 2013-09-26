06fda5bd45601076d3df56ad7784e8a18722295d
Daniel Lewis <dewis@sympletech.com>
2013-09-26 13:00:39 -0700

    Can Create an account and auth user

--- Source/my_modules/tests/userlib.tests.js
+++ Source/my_modules/tests/userlib.tests.js
@@ -16,9 +16,35 @@ describe('userlib Tests', function(){
 
             userlib.CreateAccount(newAccount, function(err, newAccount){
                 assert(newAccount._id !== null);
-                done(err);
+                userlib.DeleteUserAccount(newAccount._id, function(err, found){
+                    assert(found);
+                    done(err);
+                });
             });
         });
+
+        it('should be able to authenticate an account', function(done){
+            var newAccount = {
+                username : 'testUser',
+                password : '12345',
+                active : true
+            };
+
+            userlib.CreateAccount(newAccount, function(err, newAccount){
+                assert(newAccount._id !== null);
+
+                userlib.AuthenticateUser('testUser', '12345', function(err, isValid){
+                    assert(isValid === true);
+
+                    userlib.DeleteUserAccount(newAccount._id, function(err, found){
+                        assert(found);
+                        done(err);
+                    });
+                });
+            });
+        });
+
+
     });
 
 });
--- Source/my_modules/userlib.js
+++ Source/my_modules/userlib.js
@@ -1,11 +1,37 @@
 var userlib = function(){
     var self = this,
-        db = require('./db');
+        db = require('./db'),
+        cryptools = require('cryptools');
 
     self.CreateAccount = function(accountSettings, done){
+        accountSettings.password = cryptools.sha256(accountSettings.password);
+
         var newAccount = db.userAccountModel(accountSettings);
         newAccount.save(function(err, newAccount){
             done(err, newAccount);
+        });
+    };
+
+    self.AuthenticateUser = function(username, password, done){
+        password = cryptools.sha256(password);
+        var accountLookup = db.userAccountModel
+            .findOne({
+                'username' : username,
+                'password' : password,
+                'active'   : true},
+            function(err, account){
+                done(err, account != null);
+
+            });
+    };
+
+    self.DeleteUserAccount = function(id, done){
+        db.userAccountModel.findById(id, function(err, account){
+            var found = account !== null;
+            if(found){
+                account.remove();
+            }
+            done(err, found);
         });
     };
 
